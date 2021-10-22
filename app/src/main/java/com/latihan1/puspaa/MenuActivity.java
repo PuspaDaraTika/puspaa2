93e12c5bfd72b539199dd4bfb44a59321d2897ed
USER <puspadaratika@gmail.com>
2021-10-22 19:35:54 +0700

    logic

--- app/src/main/java/com/latihan1/puspaa/MenuActivity.java
+++ app/src/main/java/com/latihan1/puspaa/MenuActivity.java
@@ -10,21 +10,71 @@ import android.widget.ImageView;
 public class MenuActivity extends AppCompatActivity {
 
 
-    ImageView IvMenu;
+    ImageView IvTelepon;
+    ImageView IvSalad;
+    ImageView IvPizza;
+    ImageView IvFoodpack;
+    ImageView IvFastfood;
+    ImageView IvAyam;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        IvMenu = findViewById(R.id.iv_menu);
+        IvTelepon = findViewById(R.id.iv_telpon);
+        IvSalad = findViewById(R.id.iv_salad);
+        IvPizza = findViewById(R.id.iv_pizza);
+        IvFoodpack = findViewById(R.id.iv_foodpack);
+        IvFastfood = findViewById(R.id.iv_fastfood);
+        IvAyam = findViewById(R.id.iv_ayam);
 
 
-        IvMenu.setOnClickListener(new View.OnClickListener() {
+        IvTelepon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenuActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
+        IvSalad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        IvPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        IvFoodpack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        IvFastfood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        IvAyam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
